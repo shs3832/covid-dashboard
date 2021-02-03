665957f5bb2d14a7195f0db3b97501472f27defe
shs <shs3832@gmail.com>
2021-02-03 17:16:19 +0900

    - 배포 확인

--- src/api/index.js
+++ src/api/index.js
@@ -3,32 +3,17 @@ import axios from "axios";
 // const baseUrl = "https://api.corona-19.kr";
 const keyValue = "f4e2972b8fe25c7ab914e1fc3584edb40";
 
-// const instance = axios.create({
-//     baseURL: baseUrl,
-// });
-
-// instance.interceptors.request.use(
-//     function(config) {
-//         return config;
-//     },
-//     function(error) {
-//         return Promise.reject(error);
-//     }
-// );
-
 function getCovidData() {
-    // return instance.get(`/korea/?serviceKey=${keyValue}`);
     return axios({
         method: "GET",
-        url: `/korea/?serviceKey=${keyValue}`,
+        url: `https://api.corona-19.kr/korea/?serviceKey=${keyValue}`,
     });
 }
 
 function getCityCovidData() {
-    // return instance.get(`/korea/country/new/?serviceKey=${keyValue}`);
     return axios({
         method: "GET",
-        url: `/korea/country/new/?serviceKey=${keyValue}`,
+        url: `https://api.corona-19.kr/korea/country/new/?serviceKey=${keyValue}`,
     });
 }
 
